a9bf42b708954cf124610c47094b50d5cbadf04e
Šimon Tupý <simontupy64@gmail.com>
2023-03-27 21:21:40 +0200

    Added a simple test script that restarts VS Code.

---
+++ test.bat
@@ -0,0 +1,23 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+set "vscode_path=%USERPROFILE%\.vscode\extensions"
+set "source_folder=channel-syntax-support"
+set "current_path=%~dp0"
+set "folder_path=%vscode_path%\%source_folder%"
+
+if exist "%vscode_path%" (
+    if exist "%source_folder%" (
+        if exist "%folder_path%" (
+            RD /S /Q "%folder_path%"
+        )
+
+        xcopy /E /I /Y "%source_folder%" "%folder_path%" 
+        start cmd.exe /c "taskkill /IM Code.exe /F & timeout /t 0 /nobreak >nul & code ""%current_path%"" & exit"
+    )
+) else (
+    echo visual studio code not found
+)
+
+exit
